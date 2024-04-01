d0e09194e98cdc127768634119daa7fd0bfb143f
Siddharth Subramanian <sidsbrmnn@gmail.com>
2024-04-01 12:01:08 -0700

    establish a connection

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,11 @@
+using Unity.NetCode;
+
+[UnityEngine.Scripting.Preserve]
+public class GameBootstrap : ClientServerBootstrap
+{
+    public override bool Initialize(string defaultWorldName)
+    {
+        AutoConnectPort = 7979;
+        return base.Initialize(defaultWorldName);
+    }
+}
